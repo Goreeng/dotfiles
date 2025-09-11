1f6ca73e503670e4fd8c568496c2091a0202f010
Denzel Wendy <denzel.wendy@gmail.com>
2025-09-11 10:48:10 +0700

    Add TODO orgfile

--- dot_config/doom/custom.el
+++ dot_config/doom/custom.el
@@ -4,7 +4,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(deft-directory "/home/wendy/OrgFiles/"))
+ '(deft-directory "/home/wendy/OrgFiles/")
+ '(org-agenda-files '("~/WorkOrgs"))
+ '(org-capture-templates
+   '(("t" "Personal todo" entry (file+olp+datetree +org-capture-todo-file)
+      "* TODO %?" :prepend t :jump-to-captured t)
+     ("n" "Personal notes" entry (file+headline +org-capture-notes-file "Inbox")
+      "* %u %?\12%i\12%a" :prepend t)))
+ '(org-directory "~/WorkOrgs"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
