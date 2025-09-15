f5d6329689159259af9b37e2a8bb0f8ef7c0b018
Denzel Wendy <denzel.wendy@gmail.com>
2025-09-15 10:00:55 +0700

    doom update configs

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -3,6 +3,9 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(when (file-exists-p custom-file)
+  (load custom-file 'noerror))
+
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
@@ -40,7 +43,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/WorkOrgs/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
--- dot_config/doom/custom.el
+++ dot_config/doom/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-file "/home/wendy/.config/doom/custom.el")
  '(deft-directory "/home/wendy/OrgFiles/")
  '(org-agenda-files '("~/WorkOrgs"))
  '(org-capture-templates
